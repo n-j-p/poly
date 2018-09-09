390f521dae71526a074ccc5bb22c58d37402c091
NJP <noreply@github.com>
2018-09-09 15:14:04 +1000

    allow Fractions as coefficients. There is some uncertainty about how/when methods determine a integer and Fraction operation is occurring

--- poly.py
+++ poly.py
@@ -3,9 +3,9 @@ class Poly:
     ### leading coefficient (highest power of x) at position zero.
     def __init__(self, coeffs):
         assert coeffs.__class__ == list or coeffs.__class__ == tuple
-        for a in coeffs:
-            if a.__class__ != int:
-                raise TypeError ('Poly only currently implemented for ints')
+        #for a in coeffs:
+        #    if a.__class__ != int:
+        #        raise TypeError ('Poly only currently implemented for ints')
         while len(coeffs) > 0:
             if coeffs[0] == 0:
                 coeffs.pop(0)
@@ -14,7 +14,6 @@ class Poly:
         self.c = coeffs
         self.deg = len(coeffs)-1
         self.len = self.deg + 1
-        self.content = 'int'
     def __repr__(self):
         def nxt_nz(alist, cur_ix):
             for i,x in enumerate(alist[(cur_ix+1):]):
@@ -25,7 +24,7 @@ class Poly:
             return "0"
         elif len(self.c) == 1:
             return str(self.c[0]) # constant (non-zero) polynomial
-        else: # second or higher
+        else: # first degree or higher
             # First write the leading term:
             p = self.deg
             if p == 1:
@@ -34,14 +33,14 @@ class Poly:
                 elif self.c[0] == -1:
                     rep = "-x"
                 else:
-                    rep = "%dx" % self.c[0]
+                    rep = str(self.c[0]) + "x"
             else:
                 if self.c[0] == 1:
-                    rep = "x**%d" % p
+                    rep = "x**" + str(p)
                 elif self.c[0] == -1:
-                    rep = "-x**%d" % p
+                    rep = "-x**" + str(p)
                 else:
-                    rep = "%dx**%d" % (self.c[0], self.deg)
+                    rep = str(self.c[0]) + "x**" + str(self.deg)
             # Find the index of next non-zero term:
             nxt = nxt_nz(self.c, 0)
             while not nxt is None:
@@ -52,17 +51,17 @@ class Poly:
                 a = abs(self.c[nxt])
                 p = self.deg - nxt
                 if p == 0:
-                    rep += "%d" % a
+                    rep += str(a)
                 elif p == 1:
                     if a == 1:
                         rep += "x"
                     else:
-                        rep += "%dx" % a
+                        rep += str(a) + "x"
                 else:
                     if a == 1:
-                        rep += "x**%d" % p
+                        rep += "x**" + str(p)
                     else:
-                        rep += "%dx**%d" % (a,p)
+                        rep += str(a) + "x**" + str(p)
                     
                 nxt = nxt_nz(self.c, nxt)
             return rep
@@ -110,12 +109,10 @@ class Poly:
         else:
             u = g
             v = self
-        while v != [0,]:
+        while v != 0:
             t = u.div(v)[1]
             if t.len == 0:
-                return u
-            while t.c[0] == 0:
-                t.pop(0)
+                return v
             u = v
             v = t
         return u
@@ -123,17 +120,22 @@ class Poly:
     def div(self,g):
         if len(self.c) < len(g.c):
             return Poly(()), self
-        assert g.c[0] == 1 # This ensures all coefficients of solution are
-                         # integers
         n = self.len
         nv = g.len
         r = list(self.c)
         q = []
+        g0 = g.c[0]
         for k in range(n-nv+1):
-            qa = r[k]
+            if g0 == 1:            
+                qa = r[k]
+            else:
+                try:
+                    qa = Fraction(r[k], g0)
+                except TypeError:
+                    qa = r[k] / g0
             q.append(qa)
             for j in range(1,nv):
-                r[j+k] -= qa*g.c[j]
+                r[j+k] = r[j+k] - g.c[j]*qa
         return Poly(q), Poly(r[(-nv+1):])
 
     def rem(self,g):
@@ -155,6 +157,8 @@ class Fraction:
         self.d = denom
         self.__reduce__()
     def __repr__(self):
+        if self.d == 1:
+            return str(self.n)
         return str(self.n) + '/' + str(self.d)
     def __reduce__(self): # Fix +/- and possibly reduce num. and denom. by gcd
         if self.d < 0:
@@ -165,44 +169,72 @@ class Fraction:
             return
         g = _gcd(abs(self.n), abs(self.d))
         if g > 1:
-            print(g)
             self.n //= g
             self.d //= g
 
     ### Operations
     def __mul__(self, y):
-        ans = Fraction(self.n * y.n, self.d * y.d)
+        try:
+            ans = Fraction(self.n * y.n, self.d * y.d)
+        except AttributeError: # This is intended for Frac * int
+            assert y.__class__ == int
+            ans = Fraction(self.n * y, self.d)
         ans.__reduce__()
         return ans
     def __add__(self, y):
-        ans = Fraction(self.n*y.d + y.n*self.d, self.d * y.d)
+        try:
+            ans = Fraction(self.n*y.d + y.n*self.d, self.d * y.d)
+        except AttributeError:
+            assert y.__class__ == int
+            ans = Fraction(self.n + y*self.d, self.d)
         ans.__reduce__()
         return ans
     def __sub__(self, y):
         my = y.__neg__()
         return self.__add__(my)
     def __truediv__(self, y):
+        if y.__class__ == int: # Allows Frac / int
+            return self * Fraction(1,y)
         return self * y.__rtruediv__(1)
     def __pow__(self, m):
         return Fraction(self.n ** m, self.d ** m)
 
     ### Comparisons
     def __eq__(self, y):
-        if self.n == y.n and self.d == y.d:
-            return True
-        return False
+        if y.__class__ == int:
+            if y == self.n and self.d == 1:
+                return True
+            return False
+        else:
+            if self.n == y.n and self.d == y.d:
+                return True
+            return False
     def __neq__ (self, y):
         return not self.__eq__(y)
     def __lt__(self, y):
-        return self.n * y.d < y.n * self.d
+        try:
+            ans = self.n * y.d < y.n * self.d
+        except AttributeError: # i.e. y is an int?
+            assert y.__class__ == int
+            ans = self.n < y * self.d
+        return ans
     def __gt__(self, y):
-        return self.n * y.d > y.n * self.d
+        try:
+            ans = self.n * y.d < y.n * self.d
+        except AttributeError: # i.e. y is an int?
+            ans = self.n > y * self.d
+        return ans
     def __le__(self, y):
         return not self.__gt__(y)
     def __ge__(self, y):
         return not self.__lt__(y)
     def __neg__(self):
         return Fraction(-self.n, self.d)
+    def __abs__(self):
+        if self.__lt__(0):
+            return self.__neg__()
+        return self
+        
 
     ### Allow integers as left operand for some operations:
     def __rtruediv__(self, m):
@@ -236,7 +268,20 @@ def _eeuclid(x,y):
     w = y
     g = oldg
     while w > 0:
-        q = g // w
+        try:
+            q = g // w
+        except TypeError: # assume g and/or w is a whole-number fraction 
+            if g.__class__ == int:
+                assert w.d == 1
+                gn = g
+            else:
+                gn = g.n
+            if w.__class__ == int:
+                assert g.d == 1
+                wn = w
+            else:
+                wn = w.n
+            q = gn // wn
         a = u
         b = v
         g = w
